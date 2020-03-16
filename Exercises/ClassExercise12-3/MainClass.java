ec3320cdfb4ee46d0284cec813b6a9be2e73a8f5
steliospavlo <61737694+steliospavlo@users.noreply.github.com>
2020-03-16 23:45:10 +0200

    Add files via upload

---
+++ Exercises/ClassExercise12-3/MainClass.java
@@ -0,0 +1,85 @@
+package mypackage;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        //1
+//        System.out.println("Give radius");
+//        float radius = input.nextFloat();
+//        //float pi = (float) 3.14159265d;
+//        double perimeter = 2 * radius * Math.PI;
+//        double area = Math.PI * radius * radius;
+//        System.out.printf("The perimeter is %.14f and the area is %.14f\n", perimeter,area);
+//        
+//        //2
+//        byte x = 1;
+//        byte y = 2;
+//        byte a =  (byte) (-x + x++ * 2);
+//        System.out.printf("a = %d, x = %d, y = %d\n", a, x, y);
+//        byte b =(byte) ((x++ + --y) + --y);
+//        System.out.printf("b = %d, x = %d, y = %d\n", b, x, y);
+//        byte c = (byte) (x + x);
+//        System.out.printf("c = %d, x = %d, y = %d\n", c, x, y);
+//         System.out.printf("x = %d\n", x);
+//        System.out.printf("y = %d\n", y);
+//        
+//        //3
+//        System.out.println("Give width");
+//        float width = input.nextFloat();
+//        System.out.println("Give height");
+//        float height = input.nextFloat();
+//        float area2 = width*height;
+//        float perimeter2 = 2*(width+height);
+//        System.out.printf("The Area is %5f and the peripeter is %f\n", area2, perimeter2);
+//        //4
+//        int secondsGiven = input.nextInt();
+//        short hours = (short) (secondsGiven/3600);
+//        byte mins = (byte) ((secondsGiven - (hours*3600))/60);
+//        byte seconds = (byte) (secondsGiven-((hours*3600)+(mins*60)));
+//        System.out.printf("The second given are %d hours, %d minutes and %d seconds ", hours, mins, seconds);
+
+
+//        ArrayList<String> nameList = new ArrayList();
+//        ArrayList<Byte> ageList = new ArrayList();
+//
+//        for (byte i = 0; i < 3; i++) {
+//            System.out.printf("What is your name?\n");
+//            nameList.add(input.next());
+//            System.out.printf("What is your age?\n");
+//            ageList.add(input.nextByte());
+//        }
+//        for (byte i = 0; i < 3; i++) {
+//            System.out.printf("Your name is %s and your age is %d\n", nameList.get(i), ageList.get(i));
+//        }
+
+
+//        System.out.printf("Give me your temperature in the Fahrenheit scale\n");
+//        float fahrenheit = input.nextFloat();
+//        float celsius =  (fahrenheit -32) * 5/9;
+//        System.out.printf("Your temperature in fahrenheit is : %.1f\n", fahrenheit);
+//        System.out.printf("Your temperature in celsius is : %.2f\n", celsius);
+
+
+//        System.out.printf("Give a number of inches\n");
+//        float inch = input.nextFloat();
+//        float meters = (float) (inch * 0.0254) ;
+//        System.out.printf("%.2f inches are %.2f meters\n", inch, meters);
+        
+
+//        float price = input.nextFloat();
+//        for(byte i=1; i<7; i++){
+//            String discount = Integer.toString(i);
+//            float priceAfterDiscount = price - (price *i /10);
+//            System.out.printf("The price of the product with %s0%% discount is %.2f\n", discount, priceAfterDiscount);
+//        }
+    }
+
+}
+
